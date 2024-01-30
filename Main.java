95532d03bd401ef617165e9dc526e8ca354ee33d
Yud16 <89806105+Yud16@users.noreply.github.com>
2024-01-30 15:50:41 -0800

    moved main in GetFile to main in Main

--- Main.java
+++ Main.java
@@ -34,28 +34,6 @@ public class Main extends JFrame {
         // Call the parse method and get the result
         Map<String, Info> result = parser.parse(filePath);
 
-        // Print or inspect the result
-        for (Map.Entry<String, Info> entry : result.entrySet()) {
-            //String className = entry.getKey();
-            Info classInfo = entry.getValue();
-
-            if (classInfo.getClassName() != null) {
-                System.out.println("Class: " + classInfo.getClassName());
-            } else {
-                System.out.println("Interface: " + classInfo.getInterfaceName());
-                List<String> functions = classInfo.getFunctions();
-                if (!functions.isEmpty()) {
-                    System.out.println("   Functions: " + String.join(", ", functions));
-                }
-            }
-            System.out.println("   Lines of Code: " + classInfo.getLinesOfCode());
-            System.out.println("   Parent Class: " + classInfo.getParentClass());
-            System.out.println("   Implements Interface: " + classInfo.getImplementer());
-            System.out.println("   Global Variables: " + classInfo.getGlobalVariables());
-            System.out.println("   Local Variables: " + classInfo.getLocalVariables());
-            System.out.println();
-        }
-
         //display world
     }
 }
