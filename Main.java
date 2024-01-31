099395943f02e94b82d935f54f00cd980f59dbd6
Yud16 <89806105+Yud16@users.noreply.github.com>
2024-01-30 16:25:07 -0800

    Parsing entire repo

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.Map;
 import javax.swing.*;
 import java.io.IOException;
 import java.util.Scanner;
+import java.io.File;
 
 //Story: Parse the code
 //This is a test main for my own purposes so I can run it and see what its getting
@@ -12,7 +13,7 @@ public class Main extends JFrame {
     }
     public static void main(String[] args) {
         //get github repo
-        String filePath;
+        String repoPath;
 
         Scanner scanner = new Scanner(System.in);
         // prompt end user to enter repo url
@@ -20,20 +21,33 @@ public class Main extends JFrame {
             System.out.print("Enter GitHub repository URL: ");
             String repoUrl = scanner.nextLine().trim();
 
-            filePath = GetFile.downloadAndExtractRepository(repoUrl); //get file
+            repoPath = GetFile.downloadAndExtractRepository(repoUrl); //saves repo into repoPath
+
+            File repo = new File(repoPath);
+
+            if (repo.exists() && repo.isDirectory()) {
+                File[] repoFiles = repo.listFiles(); //list of repo contents
+                for (File cities : repoFiles) {
+                    //if file is a directory, make a platform (city)
+                    if (cities.isDirectory()) {
+                        File[] city = cities.listFiles();
+                        //for each building in a city
+                        for (File building : city) {
+                            if (building.isFile()) {
+                                // Create an instance of the Parser class
+                                Parser parser = new Parser();
+
+                                // Call the parse method and get the result
+                                Map<String, Info> result = parser.parse(building.getName());
+
+                            }
+                        }
+                    }
+                }
+            }
 
         } catch (IOException e) {
             System.err.println("Error downloading repository: " + e.getMessage());
         }
-
-        // Replace "your_file_path_here" with the actual path to your file
-
-        // Create an instance of the Parser class
-        Parser parser = new Parser();
-
-        // Call the parse method and get the result
-        Map<String, Info> result = parser.parse(filePath);
-
-        //display world
     }
 }
